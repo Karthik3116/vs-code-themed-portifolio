b1e18821127e53bc5b59d4ba64fa5df7a390597f
karthik <karthik3116k@gmail.com>
2025-07-06 16:06:35 +0530

    responsive v-1

--- src/utils/useTheme.js
+++ src/utils/useTheme.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 
 export const useApplySavedTheme = () => {
   useEffect(() => {
-    const theme = localStorage.getItem("theme") || "dark";
+    const theme = localStorage.getItem("theme") || "forest";
     document.documentElement.setAttribute("data-theme", theme);
   }, []);
 };
